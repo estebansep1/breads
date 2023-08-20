672277f0f8c49feb206269bfffe2a704f03773cd
Esteban Sepulveda <tebansd@icloud.com>
2023-08-20 11:56:43 -0700

    fixed the nodemon problem

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-    // ROUTES
-    app.get('/', (req, res) => {
-        res.send('Welcome to an Awesome App about Breads')
-    })
-    
-    // Breads
-    const breadsController = require('./controllers/breads_controllers.js')
-    app.use('/breads', breadsController)
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
     
 // LISTEN
 app.listen(PORT, () => {
