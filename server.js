39f86a4e32629cf8ca78e3a7181d3c8d9180d6ee
Esteban Sepulveda <tebansd@icloud.com>
2023-08-22 20:57:49 -0700

    created and finished new routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express()
 
 // MIDDLEWARE
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -31,5 +32,5 @@ app.listen(PORT, () => {
 
 // 404 Page
 app.get('*', (req, res) => {
-  res.send('404')
+  res.render('NotFound')
 })
