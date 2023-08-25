61fe2becc31643407b4c4e2aeccd18fd5763c87b
Esteban Sepulveda <tebansd@icloud.com>
2023-08-24 19:51:32 -0700

    added edit

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -17,6 +17,13 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:arrayIndex/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.arrayIndex],
+    index: req.params.arrayIndex
+  })
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
@@ -48,6 +55,17 @@ breads.post('/', (req, res) => {
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 
