6f8a421569541403c3d02956cfc7ad5794c2223f
Esteban Sepulveda <tebansd@icloud.com>
2023-08-30 17:25:15 -0700

    edit and delete functionality

--- views/show.jsx
+++ views/show.jsx
@@ -1,9 +1,9 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread, index}) {
+function Show ({bread, id}) {
     // Confirm we are getting our bread data in the terminal.
-    console.log(index)
+    console.log('the id: ',id)
       return (
         <Default>
   <h3>{bread.name}</h3>
@@ -16,11 +16,11 @@ function Show ({bread, index}) {
     }
     have gluten.
   </p>
-  <img src={bread.image} alt={bread.name} />
-  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
-  <form action={`/breads/${index}?_method=DELETE`} method="POST">
-     <input type='submit' value="DELETE"/>
-  </form>
+  <img src={bread.id} alt={bread.name} />
+  <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+  <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
+  <input type='submit' value="DELETE"/>
+</form>
   <li><a href="/breads">Go home</a></li>
 </Default>
 
